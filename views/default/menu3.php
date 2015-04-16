8d610b5f67adc646d69872bd55d1612eeae6050f
moneya <moneya@users.noreply.github.com>
2015-04-16 18:30:10 +0000

    fixed url loop issue

--- views/default/menu3.php
+++ views/default/menu3.php
@@ -2,6 +2,6 @@
 <body bgcolor="#EEE">
 
 <!-- BEGIN - EMBED Channel FROM Come2Play.com -->
-<iframe src="http://http://game.getdino.com/socialgame.php" width="100%" height="1230" frameborder="0" scrolling="no" allowtransparency="true" ></iframe>
+<iframe src="http://game.getdino.com/socialgame.php" width="100%" height="1230" frameborder="0" scrolling="no" allowtransparency="true" ></iframe>
 <!-- END - EMBED Channel FROM Come2Play.com -->
 </body>
